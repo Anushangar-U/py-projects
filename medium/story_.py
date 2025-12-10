0b03ce9bf179c00e8acc0724e89984e4cf0af74e
Anushangar-U <u.anushangar@gmail.com>
2025-12-10 15:05:13 +0530

    finished project

--- medium/story_.py
+++ medium/story_.py
@@ -1,7 +1,7 @@
 with open("story.txt", "r") as file:
     story = file.read()
 
-words = []
+words = set()
 start_of_word = -1
 
 for i,char in enumerate(story):
@@ -9,8 +9,18 @@ for i,char in enumerate(story):
         start_of_word = i
 
     if char == ">" and start_of_word != -1:
-        word = story[start_of_word: i]
-        words.append(word)
+        word = story[start_of_word: i + 1]
+        words.add(word)
         start_of_word = -1
 
-print(words)
+anwers = {}
+
+for word in words:
+    user_input = input(f"Enter a {word}: ")
+    anwers[word] = user_input
+
+for word, user_input in anwers.items():
+    story = story.replace(word, user_input)
+
+print("\nHere is your story:\n")
+print(story)
