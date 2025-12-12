6a88c3a655e8999101297783c23805a913cf3137
Anushangar-U <u.anushangar@gmail.com>
2025-12-12 16:10:51 +0530

    updated the  question generator fucntion

--- medium/maths_quiz.py
+++ medium/maths_quiz.py
@@ -4,9 +4,24 @@ operators = ["+","-","*","/"]
 
 NUM1 = random.randint(1,100)
 NUM2 = random.randint(1,100)
+q_opearator = random.choice(operators)
 
-def logic():
-    pass
+def question_generator():
+    user_anws = input(f"what is {NUM1} {q_opearator} {NUM2}")
+    match q_opearator:
+        case "+":
+            anws = NUM1 + NUM2
+        case "-":
+            anws = NUM1 - NUM2
+        case "/":
+            anws = NUM1 / NUM2
+        case "*":
+            anws = NUM1 * NUM2
+
+    if user_anws == anws:
+        print("correct!")
+    else:
+        print(f"wrong! the correct answer is {anws}")
 
 game = input("Do you want to play a math quiz game?(y/n)").lower()
 
