a339261b853f39bbb5ccfabdb09be0dab71012c6
Anushangar-U <u.anushangar@gmail.com>
2025-12-19 17:21:25 +0530

    adding bet and lines function

--- hard/gambamachine.py
+++ hard/gambamachine.py
@@ -1,3 +1,7 @@
+MAX_LINES = 3
+MAX_BET = 10000
+MIN_BET = 1000
+
 def deposit():
     while True:
         amount = input("Enter amount to deposit: ")
@@ -8,4 +12,28 @@ def deposit():
             break
     return amount
 
- 
+def get_number_of_lines():
+    while True:
+        lines = input(f"Enter the number of lines(1 to {MAX_LINES}): ")
+        if lines >= 1 and lines<= MAX_LINES:
+            lines = int(lines)
+        else:
+            print("Enter a valid number of lines")
+            break
+    return lines
+
+def get_bet():
+    while True:
+        amount = input(f"Enter the amount you want to bet (Rs.{MIN_BET} to Rs.{MAX_LINES}): ")
+        if amount >= MIN_BET and amount<= MAX_BET:
+            amount = float(amount)
+        else:
+            print("Enter a valid amount")
+            break
+    return amount
+
+def program():
+    lines = get_number_of_lines()
+    balance = deposit()
+    bet = get_bet()
+    print(lines, balance, bet)
